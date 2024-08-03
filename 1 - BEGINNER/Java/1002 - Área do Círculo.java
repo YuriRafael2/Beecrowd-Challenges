9be0b7b0bc58cf913ac10129d521d927a36846a8
YuriRafael2 <yurioliveira8001@hotmail.com>
2024-08-03 17:26:55 -0300

    adição de novos exercicios

---
+++ 1 - BEGINNER/Java/1002 - Área do Círculo.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    
+    public static final double PI = 3.14159;
+    
+    public static void main(String[] args) throws IOException {
+
+        Scanner input = new Scanner(System.in);
+        
+        double raio = input.nextDouble();
+        double area = PI * raio * raio;
+        
+        System.out.printf("A=%.4f\n", area);
+ 
+    }
+ 
+}
---
+++ 1 - BEGINNER/Java/1004 - Produto Simples.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner input = new Scanner(System.in);
+        
+        int x = input.nextInt();
+        int y = input.nextInt();
+        int PROD = x * y;
+        
+        System.out.printf("PROD = %d\n", PROD);
+ 
+    }
+ 
+}
